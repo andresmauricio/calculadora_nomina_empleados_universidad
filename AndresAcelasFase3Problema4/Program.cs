dc32fdd1679f81dc11a1a4a5ea15fa81b8833728
andresmauricio <andresacelas@outlook.com>
2020-11-08 08:13:57 -0500

    Initial commit

---
+++ AndresAcelasFase3Problema4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndresAcelasFase3Problema4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintInformationStudent();
+            CrearTrabajador();
+            Console.ReadLine();
+
+        }
+
+        public static void CrearTrabajador()
+        {
+            Trabajador trabajador = new Trabajador();
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine("Ingrese el Nombre del trabajor");
+            trabajador.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la identificación del trabajor");
+            trabajador.NumerodeIdentificacion = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número de horas trabajadas");
+            trabajador.HorasTrabajadas = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+            trabajador.ImprimirDatosDelTrabajador();
+        }
+
+        public static void PrintInformationStudent()
+        {
+            Console.WriteLine("::: FASE 3 - MÉTODOS Y CLASES :::");
+            Console.WriteLine();
+            Console.WriteLine("NOMBRE DEL CURSO -> INTRODUCCION A LA PROGRAMACIÓN");
+            Console.WriteLine("NÚMERO DE FASE -> FASE 3 CLASES Y MÉTODOS");
+            Console.WriteLine("CÓDIGO DEL ESTUDIANTE -> 1052412239");
+            Console.WriteLine("NOMBRE DEL ESTUDIANTE -> ANDRÉS MAURICIO ACELAS AREVALO");
+            Console.WriteLine();
+            Console.WriteLine("DESCRIPCIÓN DE LA ACTIVIDAD - PROBLEMA 4");
+            Console.WriteLine("Solucionar el problema de una empresa que contrata diseñadores por horas ocacionalmete y le reañizan el pago por horas.");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AndresAcelasFase3Problema4/Trabajador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndresAcelasFase3Problema4
+{
+    class Trabajador
+    {
+        public int NumerodeIdentificacion { get; set; }
+        public string Nombre { get; set; }
+        public int HorasTrabajadas { get; set; }
+
+        public int ValorHoraTrabajador = 30000;
+
+        private double CalcularHorasTrabajadas(int numeroHoras)
+        {
+            var Resultado = ValorHoraTrabajador * numeroHoras;
+            return Resultado;
+        }
+
+        public void ImprimirDatosDelTrabajador ()
+        {
+            var Total = CalcularHorasTrabajadas(HorasTrabajadas);
+            Console.WriteLine("::: RESULTADO DEL TRABAJADOR :::");
+            Console.WriteLine("NOMBRE DEL TRABAJADOR: " + Nombre);
+            Console.WriteLine("IDENTIDICACIÓN DEL TRABAJADOR: " + NumerodeIdentificacion);
+            Console.WriteLine("HORAS TRABAJADAS: " + HorasTrabajadas);
+            Console.WriteLine("VALOR A PAGAR: $" +  Total);
+        }
+    }
+}
